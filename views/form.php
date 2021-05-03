0b1e6a67bc9a3b9230ce2cab58ea5de3d05e8089
ALI MOUSSA <moussa.ali.pro@gmail.com>
2021-05-03 10:13:29 +0000

    Formulaire de création/modification des mailing lists

--- views/form.php
+++ views/form.php
@@ -2,42 +2,45 @@
     <div class="card">
         <div class="card-header"><span class="icon-mail"></span> Mailing List</div>
         <div class="card-body">
-            <form method="<?php echo $formMethod ?>">
+            <form method="POST">
                 <div class="form-row pb-3 d-flex align-items-center">
-                    <label for="account" class="col-lg-3 text-lg-right col-form-label">Email <span class="text-danger">*</span></label>
-                    <input type="text" required disabled class="form-control col-6 col-lg-5" id="account" value="<?php echo $account ?>"><span class="col-6 col-lg-4">@<?php echo $domain ?></span>
+                    <label for="name" class="col-lg-4 text-lg-right col-form-label"><span class="text-danger">*</span> Nom : </label>
+                    <input type="text" required class="form-control col-12 col-lg-6" id="name" value="<?php echo $name ?>">
                 </div>
 
-                <div class="form-row pb-3">
-                    <label class="col-lg-3 col-form-label text-lg-right" for="content">Message <span class="text-danger">*</span></label>
-                    <textarea class="col-lg-9 form-control" required <?php if($action == 'show') echo 'disabled'; ?> rows="8" id="content" name="content"><?php echo $content ?></textarea>
+                <div class="form-row pb-3 d-flex align-items-center">
+                    <label for="ownerEmail" class="col-lg-4 text-lg-right col-form-label"><span class="text-danger">*</span> Propriétaire : </label>
+                    <input type="text" required class="form-control col-12 col-lg-6" id="ownerEmail" value="<?php echo $ownerEmail ?>">
                 </div>
                 
-                <div class="offset-lg-3 form-check pb-3 d-flex align-items-center">
-                    <input class="form-check-input" type="checkbox" <?php if($action == 'show') echo 'disabled'; ?> <?php if($copy) echo 'checked'; ?> id="copy" name="copy">
-                    <label class="form-check-label" for="copy">
-                        Sauvegarder les mails reçus
+                <div class="offset-lg-4 form-check pb-3 d-flex align-items-center">
+                    <input class="form-check-input" type="checkbox" <?php if($moderatorMessage) echo 'checked'; ?> id="moderatorMessage" name="moderatorMessage">
+                    <label class="form-check-label" for="moderatorMessage">
+                        Modérer tous les messages <span class="icon-mail" title="Le propriétaire ou un modérateur doit approuver l'e-mail envoyé à la mailing-list."></span>
                     </label>
                 </div>
 
-                <div class="form-row pb-3">
-                    <label class="col-lg-3 col-form-label text-lg-right" for="from">Début <span class="text-danger">*</span></label>
-                    <input type="date" required <?php if($action == 'show') echo 'disabled'; ?> class="col-lg-9 form-control" id="from" name="from" value="<?php echo $from ?>">
+                <div class="offset-lg-4 form-check pb-3 d-flex align-items-center">
+                    <input class="form-check-input" type="checkbox" <?php if($subscribeByModerator) echo 'checked'; ?> id="subscribeByModerator" name="subscribeByModerator">
+                    <label class="form-check-label" for="subscribeByModerator">
+                        Modération des abonnés <span class="icon-mail" title="Le propriétaire, ou un modérateur, doit approuver les inscriptions à la mailing-list."></span>
+                    </label>
                 </div>
 
-                <div class="form-row pb-3">
-                    <label class="col-lg-3 col-form-label text-lg-right" for="to">Fin (inclus) <span class="text-danger">*</span></label>
-                    <input type="date" required <?php if($action == 'show') echo 'disabled'; ?> class="col-lg-9 form-control" id="to" name="to" value="<?php echo $to ?>">
+                <div class="offset-lg-4 form-check pb-3 d-flex align-items-center">
+                    <input class="form-check-input" type="checkbox" <?php if($usersPostOnly) echo 'checked'; ?> id="usersPostOnly" name="usersPostOnly">
+                    <label class="form-check-label" for="usersPostOnly">
+                        Seuls les abonnés peuvent poster <span class="icon-mail" title="Restreint l'envoi d'e-mails sur la mailing list aux seules abonnés de celle-ci."></span>
+                    </label>
+                </div>
+
+                <div class="form-row pb-3 d-flex align-items-center">
+                    <label for="replyTo" class="col-lg-4 text-lg-right col-form-label">Répondre à : </label>
+                    <input type="email" class="form-control col-12 col-lg-6" id="replyTo" value="<?php echo $replyTo ?>">
                 </div>
 
                 <div class="form-row pb-3 d-flex">
-                    <?php if($action == 'create'){ ?>
-                        <button type="submit" class="offset-lg-3 btn btn-primary px-4" name="action" value="create">Créer</button>
-                    <?php }else if($action == 'show'){ ?>
-                        <button type="submit" class="offset-lg-3 btn btn-danger px-4" name="action" value="delete">Supprimer</button>
-                    <?php } ?>
-
-                    <!-- <a class="ml-auto btn btn-secondary" href="/"><span class="icon-home"></span>Accueil</a> -->
+                    <button type="submit" class="offset-lg-4 btn btn-primary px-4" name="action" value="create">Valider</button>
                 </div>
             </form>
         </div>
